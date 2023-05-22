0003bafb1781c09c03408b87086584541f2a7b41
Georgia <creativity2030@live.com>
2023-05-22 10:00:58 -0300

    sfsdf

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -18,49 +18,25 @@ export const Experience = () => {
       style={{ height: "100vh" }}
     >
       <Suspense fallback={null}>
-        <ScrollControls horizontal pages={3}>
-          <Scroll>
-            <ambientLight intensity={0.5} />
-            <spotLight
-              position={[10, 10, 10]}
-              angle={0.15}
-              penumbra={1}
-              shadow-mapSize={[512, 512]}
-              castShadow
-            />
+        <ambientLight intensity={0.5} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          shadow-mapSize={[512, 512]}
+          castShadow
+        />
 
-            <Scene setSelectedModel={setSelectedModel} />
-            <ContactShadows
-              rotation-x={Math.PI / 2}
-              position={[0, 0, 0]}
-              opacity={0.75}
-              width={200}
-              height={10}
-              blur={5}
-              far={10}
-            />
-          </Scroll>
-          {/* <Scroll html>
-            <h1 style={{ position: "absolute", top: "20vh", left: "-75vw" }}>
-            home
-            </h1>
-            <h1 style={{ position: "absolute", top: "20vh", left: "25vw" }}>
-            to
-            </h1>
-            <h1 style={{ position: "absolute", top: "20vh", left: "125vw" }}>
-            be
-            </h1>
-            <h1 style={{ position: "absolute", top: "20vh", left: "225vw" }}>
-            home
-            </h1>
-            <h1 style={{ position: "absolute", top: "20vh", left: "325vw" }}>
-            to
-            </h1>
-            <h1 style={{ position: "absolute", top: "20vh", left: "425vw" }}>
-            be
-            </h1>
-          </Scroll> */}
-        </ScrollControls>
+        <Scene setSelectedModel={setSelectedModel} />
+        <ContactShadows
+          rotation-x={Math.PI / 2}
+          position={[0, 0, 0]}
+          opacity={0.75}
+          width={200}
+          height={10}
+          blur={5}
+          far={10}
+        />
         <Preload />
       </Suspense>
     </Canvas>
